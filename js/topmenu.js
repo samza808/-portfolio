2b11e5029bf242dadb518f496e8cb751d759ae43
kwonSuYoun <balenti808@gmail.com>
2022-07-14 15:21:09 +0900

    test-1

--- js/topmenu.js
+++ js/topmenu.js
@@ -2,6 +2,12 @@
 const menuBar = document.querySelector('#topmenu > div');
 console.log(menuBar);
 
+menuBar.style.width = '900px';
+menuBar.style.height = '30px';
+menuBar.style.display = 'flex';
+menuBar.style.justifyContent = 'space-between';
+menuBar.style.alignItems = 'center';
+
 console.log(menuBar.children[0]);
 
 for (let a = 0; a < menuBar.children.length; a++) {
