511278d23564e24513229dbc9829b9c8422e4c8b
kwonSuYoun <balenti808@gmail.com>
2022-07-14 17:40:10 +0900

    탑메뉴 js

--- js/topmenu.js
+++ js/topmenu.js
@@ -10,6 +10,26 @@ menuBar.style.alignItems = 'center';
 
 console.log(menuBar.children[0]);
 
+console.dir(menuBar.children[0].style);
+
+let hover = true;
+
 for (let a = 0; a < menuBar.children.length; a++) {
   menuBar.children[a].style.cursor = 'pointer';
+  menuBar.children[a].style.width = '154px';
+  menuBar.children[a].style.height = '100%';
+  menuBar.children[a].style.fontSize = '24px';
+  menuBar.children[a].style.fontWeight = 'bold';
+  menuBar.children[a].style.display = 'flex';
+  menuBar.children[a].style.justifyContent = 'center';
+  menuBar.children[a].style.fontFamily = 'Balsamiq Sans, cursive';
+
+  menuBar.children[a].addEventListener('mouseover', function(event) {
+    event.target.style.color = '#4F9CAA';
+  } );
+
+  menuBar.children[a].addEventListener('mouseout', function(event) {
+    event.target.style.color = '#484848';
+  } );
+
 }
